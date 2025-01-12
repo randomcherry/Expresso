281ecc05c2ef8d761820badd9f6d46079830a067
cherry <130264633+randomcherry@users.noreply.github.com>
2025-01-12 19:33:03 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // You can add your JavaScript code here
+    console.log('Web app is loaded!');
+});
+
+function getStarted() {
+    window.location.href = 'youtube.com';
+}

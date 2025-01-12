02414acaf0c8baed37fdf04b76b09f1811c9a0dd
cherry <130264633+randomcherry@users.noreply.github.com>
2025-01-12 19:43:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function getStarted() {
-    window.location.href = 'youtube.com';
+    window.location.href = 'homeScreen.html';
 }
